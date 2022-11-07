6c3303189c0b3cfcd61d4c80b0da7e38ffedc761
Bruno Monteiro <104601906+Brunomon2812@users.noreply.github.com>
2022-11-07 16:11:57 -0500

    Base URL update

--- case-ambulnz-frontend/src/constants/constants.js
+++ case-ambulnz-frontend/src/constants/constants.js
@@ -1,4 +1,4 @@
-export const BASE_URL = "http://ec2-44-204-57-105.compute-1.amazonaws.com/api"
+export const BASE_URL = "http://ec2-52-23-254-87.compute-1.amazonaws.com/api"
 
 // GET http://localhost:3003/api/pizzas
 // GET http://localhost:3003/api/orders
